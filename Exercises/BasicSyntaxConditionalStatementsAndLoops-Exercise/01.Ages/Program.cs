1e5944862f27019e593a736b5eed80e850363cca
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-16 23:26:22 +0300

    Add files via upload

---
+++ Exercises/BasicSyntaxConditionalStatementsAndLoops-Exercise/01.Ages/Program.cs
@@ -0,0 +1,22 @@
+﻿int years = int.Parse(Console.ReadLine());
+
+if (years >= 0 && years < 3)
+{
+    Console.WriteLine("baby");
+}
+else if (years > 2 && years < 14)
+{
+    Console.WriteLine("child");
+}
+else if (years > 13 && years < 20)
+{
+    Console.WriteLine("teenager");
+}
+else if (years > 19 && years < 66)
+{
+    Console.WriteLine("adult");
+}
+else if(years > 65)
+{
+    Console.WriteLine("elder");
+}
