76b3d31fe4ea9b4dd8e3cccae762536f9486422f
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-16 23:29:06 +0300

    Add files via upload

---
+++ Exercises/BasicSyntaxConditionalStatementsAndLoops-Exercise/05.Login/Program.cs
@@ -0,0 +1,35 @@
+﻿string username = Console.ReadLine();
+
+//char[] charArray = username.ToCharArray();
+//Array.Reverse(charArray);
+//var stringReverse = new string(charArray);
+
+string password = "";
+for (int i = username.Length-1; i >= 0; i--)
+{
+    password += username[i];
+}
+
+int attempts = 4;
+
+while (attempts > 0)
+{
+    attempts -= 1;
+    string typedPassword = Console.ReadLine();
+    if (password == typedPassword)
+    {
+        Console.WriteLine($"User {username} logged in.");
+        break;
+    }
+    else
+    {
+        if (attempts == 0)
+        {
+            Console.WriteLine($"User {username} blocked!");
+        }
+        else
+        {
+            Console.WriteLine("Incorrect password. Try again.");
+        }
+    }
+}
