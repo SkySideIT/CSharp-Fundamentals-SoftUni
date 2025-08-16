bc7c906508d775703b192411255b35eb3f12395c
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-16 23:33:14 +0300

    Add files via upload

---
+++ Exercises/BasicSyntaxConditionalStatementsAndLoops-Exercise/11.Orders/Program.cs
@@ -0,0 +1,22 @@
+﻿int orders = int.Parse(Console.ReadLine());
+
+double currentTotal = 0;
+double total = 0;
+for (int i = 0; i < orders; i++)
+{
+    double pricePerCapsule = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsulesCount = int.Parse(Console.ReadLine());
+    currentTotal = pricePerCapsule * days * capsulesCount;
+    if (orders == 1)
+    {
+        total = currentTotal;
+    }
+    else
+    {
+        total += currentTotal;
+    }
+    Console.WriteLine($"The price for the coffee is: ${currentTotal:f2}");
+}
+
+Console.WriteLine($"Total: ${total:f2}");
