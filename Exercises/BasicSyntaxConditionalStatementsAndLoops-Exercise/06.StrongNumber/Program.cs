d818cded38fc61b30651d42eff046c681a01a621
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-16 23:30:26 +0300

    Add files via upload

---
+++ Exercises/BasicSyntaxConditionalStatementsAndLoops-Exercise/06.StrongNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int numberCopy = number;
+int factSum = 0;
+
+while (numberCopy > 0)
+{
+    int digit = numberCopy % 10;
+    numberCopy /= 10;
+
+    int fact = 1;
+    for (int i = 1; i <= digit; i++)
+    {
+        fact *= i;
+    }
+
+    factSum += fact;
+}
+
+bool isStrong = factSum == number;
+
+Console.WriteLine(isStrong ? "yes" : "no");
