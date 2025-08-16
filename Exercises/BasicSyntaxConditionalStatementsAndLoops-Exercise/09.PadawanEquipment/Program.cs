cb54dfd452701dd1651e19e5cea91665ea6237c4
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-16 23:32:17 +0300

    Add files via upload

---
+++ Exercises/BasicSyntaxConditionalStatementsAndLoops-Exercise/09.PadawanEquipment/Program.cs
@@ -0,0 +1,24 @@
+﻿double moneyAmount = double.Parse(Console.ReadLine());
+int studentsCount = int.Parse(Console.ReadLine());
+double saberPrice = double.Parse(Console.ReadLine());
+double robePrice = double.Parse(Console.ReadLine());
+double beltPrice = double.Parse(Console.ReadLine());
+
+double percent = (double)studentsCount * 0.10;
+double allSabers = studentsCount + Math.Ceiling(percent);
+double sabers = saberPrice * allSabers;
+
+double robes = studentsCount * robePrice;
+
+double beltsDiscount = studentsCount - Math.Ceiling((double)(studentsCount / 6));
+double belts = beltPrice * beltsDiscount;
+
+double total = sabers + robes + belts;
+if (total <= moneyAmount)
+{
+    Console.WriteLine($"The money is enough - it would cost {total:f2}lv.");
+}
+else
+{
+    Console.WriteLine($"John will need {(total - moneyAmount):f2}lv more.");
+}
