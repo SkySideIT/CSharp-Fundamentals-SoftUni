e78e1d50cfeff2ce35897ad160e47927671ed900
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-18 22:57:01 +0300

    Add files via upload

---
+++ Exercises/Arrays-Exercise/01.Train/Program.cs
@@ -0,0 +1,17 @@
+﻿int wagons = int.Parse(Console.ReadLine());
+int sum = 0;
+int[] peopleArr = new int[wagons];
+
+for (int i = 0; i < wagons; i++)
+{
+    int people = int.Parse(Console.ReadLine());
+    peopleArr[i] = people;
+    sum += people;
+}
+
+for (int i = 0; i < peopleArr.Length; i++)
+{
+    Console.Write(peopleArr[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine(sum);
