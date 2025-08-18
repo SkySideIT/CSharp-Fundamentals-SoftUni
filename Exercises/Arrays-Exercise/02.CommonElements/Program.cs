ee1b0af3ea5dcf164aae0d2878290d5e0304c747
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-18 22:58:13 +0300

    Add files via upload

---
+++ Exercises/Arrays-Exercise/02.CommonElements/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+
+Hey hello 2 4
+10 hey 4 hello
+
+*/
+
+string[] firstArr = Console.ReadLine().Split();
+string[] secondArr = Console.ReadLine().Split();
+string[] result = {};
+
+for (int i = 0; i < secondArr.Length; i++)
+{
+	for (int j = 0; j < firstArr.Length; j++)
+	{
+		if (firstArr[j] == secondArr[i])
+		{
+			Console.Write(firstArr[j] + " ");
+        }
+	}
+}
