af3ac7e53e7c2a6a36e203fb0e2178adc35560de
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-18 22:58:52 +0300

    Add files via upload

---
+++ Exercises/Arrays-Exercise/03.ZigZagArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿int lines = int.Parse(Console.ReadLine());
+int[] firstNums = new int[lines];
+int[] secondNums = new int[lines];
+
+for (int i = 0; i < lines; i++)
+{
+    int[] inputs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+	if (i == 0 || i % 2 == 0)
+	{
+		firstNums[i] = inputs[0];
+		secondNums[i] = inputs[1];
+	}
+	else
+	{
+		firstNums[i] = inputs[1];
+		secondNums[i] = inputs[0];
+	}
+}
+
+for (int i = 0; i < firstNums.Length; i++)
+{
+	Console.Write(firstNums[i] + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < secondNums.Length; i++)
+{
+    Console.Write(secondNums[i] + " ");
+}
