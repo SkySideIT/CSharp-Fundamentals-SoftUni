bd475c4ee5129399e72350d517c7911cde88d4dc
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-18 23:28:19 +0300

    Add files via upload

---
+++ Exercises/Arrays-Exercise/08.MagicSum/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] numsArr = Console.ReadLine().Split();
+int[] nums  = new int[numsArr.Length];
+for (int i = 0; i < numsArr.Length; i++)
+{
+    nums[i] = int.Parse(numsArr[i]);
+}
+int result = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < nums.Length; i++)
+{
+	for (int j = i + 1; j < nums.Length; j++)
+	{
+		if (nums[i] + nums[j] == result)
+		{
+			Console.Write(nums[i] + " " + nums[j]);
+            Console.WriteLine();
+        }
+	}
+}
