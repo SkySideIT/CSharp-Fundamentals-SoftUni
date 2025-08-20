403646a6f9fdb3fcfc009a9a0effa66c343c5945
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-20 19:04:25 +0300

    Add files via upload

---
+++ Exercises/Methods-Exercise/07.NxNMatrix/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace _07.NxNMatrix
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            GetMatrix(number);
+        }
+
+        static void GetMatrix(int number)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write(number + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
