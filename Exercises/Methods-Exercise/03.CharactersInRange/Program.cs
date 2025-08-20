61399e2b4abcd959291e0f9d8075b48485c25d4b
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-20 18:59:33 +0300

    Add files via upload

---
+++ Exercises/Methods-Exercise/03.CharactersInRange/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace _03.Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                char temp = a;
+                b = a;
+                a = temp;
+            }
+
+            PrintBetween(a, b);
+        }
+
+        static void PrintBetween(char a, char b)
+        {
+            for (int i = a + 1; i < b; i++)
+            {
+                Console.Write($"{(char)i} ");
+            }
+        }
+    }
+}
