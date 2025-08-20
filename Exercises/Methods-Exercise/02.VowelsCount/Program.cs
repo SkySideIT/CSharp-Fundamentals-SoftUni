34195b2b7fe622a7fa024256fea2110cff054242
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-20 18:58:43 +0300

    Add files via upload

---
+++ Exercises/Methods-Exercise/02.VowelsCount/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace _02.Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int vowelsCount = GetVolewsCount(input);
+            Console.WriteLine(vowelsCount);
+        }
+
+        static int GetVolewsCount(string input)
+        {
+            int vowelsCount = 0;
+            input = input.ToLower();
+
+            foreach (char c in input)
+            {
+                if (c == 'e' || c == 'o' || c == 'i' || c == 'u' || c == 'a')
+                {
+                    vowelsCount++;
+                }
+            }
+            return vowelsCount;
+        }
+    }
+}
