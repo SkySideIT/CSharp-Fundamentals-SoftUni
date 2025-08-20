5253fb180cd54315b014938fc1011d807b1f36e3
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-20 19:03:46 +0300

    Add files via upload

---
+++ Exercises/Methods-Exercise/06.MiddleCharacters/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace _06.MiddleCharacters
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            char[] middle = GetMiddle(str);
+            for (int i = 0; i < middle.Length; i++)
+            {
+                Console.Write(middle[i]);
+            }
+        }
+
+        private static char[] GetMiddle(string str)
+        {
+            char[] strNew = str.ToCharArray();
+            char[] middle = new char[strNew.Length];
+            if (str.Length % 2 == 0)
+            {
+                middle[0] = strNew[(strNew.Length / 2) - 1];
+                middle[1] = strNew[strNew.Length / 2];
+            }
+            else
+            {
+                middle[0] = strNew[strNew.Length / 2];
+            }
+            return middle;
+        }
+    }
+}
