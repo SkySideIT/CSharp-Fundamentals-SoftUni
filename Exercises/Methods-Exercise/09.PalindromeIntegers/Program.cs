04ae0ce10422f36829f15ea0f2ad106982d03719
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-20 19:05:57 +0300

    Add files via upload

---
+++ Exercises/Methods-Exercise/09.PalindromeIntegers/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace _09.PalindromeIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberAsString;
+            while ((numberAsString = Console.ReadLine()) != "END")
+            {
+                Console.WriteLine(IsPalindrome(numberAsString));
+            }
+        }
+
+        static bool IsPalindrome(string palindromeString)
+        {
+            string reversedString = Reverse(palindromeString);
+            return palindromeString == reversedString;
+        }
+
+        static string Reverse(string palindromeString)
+        {
+            char[] ch = palindromeString.ToCharArray();
+            Array.Reverse(ch);
+            return new string(ch);
+        }
+    }
+}
