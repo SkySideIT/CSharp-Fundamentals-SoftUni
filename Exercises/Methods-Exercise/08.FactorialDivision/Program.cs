8f7f3ba729a55918bba83c41499a37b50466b8bc
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-20 19:05:10 +0300

    Add files via upload

---
+++ Exercises/Methods-Exercise/08.FactorialDivision/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace _08.FactorialDivision
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double num1 = int.Parse(Console.ReadLine());
+            double num2 = int.Parse(Console.ReadLine());
+            double factNum1 = GetFactorial(num1);
+            double factNum2 = GetFactorial(num2);
+            Console.WriteLine($"{(factNum1 / factNum2):f2}");
+        }
+
+        private static double GetFactorial(double num1)
+        {
+            double fact = 1;
+            for (int i = 1; i <= num1; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+    }
+}
