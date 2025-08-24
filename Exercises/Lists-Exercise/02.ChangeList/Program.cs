bb011ed6d6b3864a4f25701e7edbe31e27b251b0
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-24 18:09:32 +0300

    Add files via upload

---
+++ Exercises/Lists-Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] arguments = input.Split();
+                int element = int.Parse(arguments[1]);
+
+                if (arguments[0] == "Insert")
+                {
+                    int index = int.Parse(arguments[2]);
+                    list.Insert(index, element);
+                }
+                else if (arguments[0] == "Delete")
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        list.Remove(element);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
