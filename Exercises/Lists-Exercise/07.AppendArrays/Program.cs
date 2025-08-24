044c7f735348a67660f60b9107c7a1421161b8dd
Danail Georgiev <105545729+SkySideIT@users.noreply.github.com>
2025-08-24 18:13:14 +0300

    Add files via upload

---
+++ Exercises/Lists-Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+1 2 3 |4 5 6 | 7 8
+ 
+ */
+
+namespace _07.AppendArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> result = new List<string>();
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                string[] updatedArray = array[i]
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+                result.AddRange(updatedArray);
+            }
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
